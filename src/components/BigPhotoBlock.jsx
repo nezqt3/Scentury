292e3aca64917d3ca6c21772a7486c091f3c525a
Denis <dalekseenko8@gmail.com>
2025-11-08 00:13:36 +0300

    add adaprive (have bug in start screen)

--- src/components/BigPhotoBlock.jsx
+++ src/components/BigPhotoBlock.jsx
@@ -1,8 +1,17 @@
 export default function BigPhotoBlock() {
-    return <div className="big-block">
-            <p>
-                Ароматы вне времени. Мы собрали коллекцию парфюмерии, которую <span>вы не встретите в масс-маркете.</span>
-            </p>
-        <img src={require("../static/bigphoto.png")} alt="big-photo"/>
+  function isMobileDevice() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent,
+    );
+  }
+
+  return (
+    <div className="big-block">
+      <p>
+        Ароматы вне времени. Мы собрали коллекцию парфюмерии, которую{" "}
+        <span>вы не встретите в масс-маркете.</span>
+      </p>
+      <img src={require("../static/bigphoto.png")} alt="big-photo" />
     </div>
+  );
 }
